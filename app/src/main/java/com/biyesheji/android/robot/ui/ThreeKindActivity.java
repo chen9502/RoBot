7798b6e350e8a3543a0bdeefb2a600dd3e9f6803
jw <1063738622@qq.com>
2018-03-08 10:23:51 +0800

    viewHolderutils

--- app/src/main/java/com/biyesheji/android/robot/ui/ThreeKindActivity.java
+++ app/src/main/java/com/biyesheji/android/robot/ui/ThreeKindActivity.java
@@ -2,10 +2,13 @@ package com.biyesheji.android.robot.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.biyesheji.android.robot.R;
 
 public class ThreeKindActivity extends AppCompatActivity {
+
+    private TextView xingjinTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
